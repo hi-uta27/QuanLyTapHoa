46b20481bf741e5b9752b283544c06716ae7896a
hi-uta27 <tavanhieun@gmail.com>
2023-03-18 09:12:51 +0700

    create fragment for main

---
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/home/fragments/HomeFragment.kt
@@ -0,0 +1,7 @@
+package com.tavanhieu.quanlytaphoa.activities.home.fragments
+
+import androidx.fragment.app.Fragment
+import com.tavanhieu.quanlytaphoa.R
+
+class HomeFragment: Fragment(R.layout.fragment_home) {
+}
---
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/statistics/fragments/StatisticsFragment.kt
@@ -0,0 +1,7 @@
+package com.tavanhieu.quanlytaphoa.activities.statistics.fragments
+
+import androidx.fragment.app.Fragment
+import com.tavanhieu.quanlytaphoa.R
+
+class StatisticsFragment: Fragment(R.layout.fragment_statistics) {
+}
