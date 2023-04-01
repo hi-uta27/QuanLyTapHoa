056cad3b80b6646fbeb2640258d9aa4e9b463eb2
hi-uta27 <tavanhieun@gmail.com>
2023-04-01 15:20:01 +0700

    create class CartActivity

---
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/cart/presentations/CartActivity.kt
@@ -0,0 +1,18 @@
+package com.tavanhieu.quanlytaphoa.activities.cart.presentations
+
+import com.tavanhieu.quanlytaphoa.R
+import com.tavanhieu.quanlytaphoa.commons.base.BaseActivity
+
+class CartActivity : BaseActivity() {
+    override fun setContentView() {
+        setContentView(R.layout.activity_cart)
+    }
+
+    override fun mappingViewId() {
+        //
+    }
+
+    override fun configLayout() {
+        //
+    }
+}
