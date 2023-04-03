7d1abf38d004d496a6fc9b63663cbe145c3e839e
hi-uta27 <tavanhieun@gmail.com>
2023-04-03 19:34:52 +0700

    design order activity with xml

---
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/order/presentations/OrderActivity.kt
@@ -0,0 +1,18 @@
+package com.tavanhieu.quanlytaphoa.activities.order.presentations
+
+import com.tavanhieu.quanlytaphoa.R
+import com.tavanhieu.quanlytaphoa.commons.base.BaseActivity
+
+class OrderActivity : BaseActivity() {
+    override fun setContentView() {
+        setContentView(R.layout.activity_order)
+    }
+
+    override fun mappingViewId() {
+        //
+    }
+
+    override fun configLayout() {
+        //
+    }
+}
