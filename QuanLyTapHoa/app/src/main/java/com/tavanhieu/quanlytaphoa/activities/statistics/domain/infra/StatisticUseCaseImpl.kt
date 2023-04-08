7212b4bcf23de78ac8f47917790d7ef5eab0da39
hi-uta27 <tavanhieun@gmail.com>
2023-04-08 21:26:42 +0700

    load data for bar chat in statistic use case and apply it

--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/statistics/domain/infra/StatisticUseCaseImpl.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/statistics/domain/infra/StatisticUseCaseImpl.kt
@@ -2,7 +2,8 @@ package com.tavanhieu.quanlytaphoa.activities.statistics.domain.infra
 
 import com.tavanhieu.quanlytaphoa.activities.statistics.domain.enum.TimeUnit
 import com.tavanhieu.quanlytaphoa.activities.statistics.domain.use_cases.StatisticUseCase
-import com.tavanhieu.quanlytaphoa.commons.compareDate
+import com.tavanhieu.quanlytaphoa.commons.*
+import com.tavanhieu.quanlytaphoa.commons.models.Cart
 import com.tavanhieu.quanlytaphoa.commons.models.Order
 import com.tavanhieu.quanlytaphoa.data_network_layer.FirebaseNetworkLayer
 import java.util.*
@@ -23,37 +24,113 @@ class StatisticUseCaseImpl: StatisticUseCase {
                     entities.add(order)
                 }
             }
-            filterBy(entities, timeLine, timeUnit, complete, failure)
+            filter(entities, timeLine, timeUnit, complete, failure)
         }, failure)
     }
 
-    private fun filterBy(
+    private fun filter(
         entities: ArrayList<Order>,
         timeLine: Date,
         timeUnit: TimeUnit,
         complete: (ArrayList<Order>) -> Unit,
         failure: () -> Unit
     ) {
-        var results: ArrayList<Order> = ArrayList()
+        // day -> order in day
+        // weak -> all order in day
+        // month -> all order in day
+        // year -> all order in month
         when (timeUnit) {
-            TimeUnit.day -> {
-                results = entities.filter { timeLine.compareDate(it.date) } as ArrayList<Order>
-                complete(results)
-            }
-//            TimeUnit.weak -> {
-//                results = entities.filter { it.date == timeLine } as ArrayList<Order>
-//                complete(results)
-//            }
-//            TimeUnit.month -> {
-//                results = entities.filter { it.date == timeLine } as ArrayList<Order>
-//                complete(results)
-//            }
-//            TimeUnit.year -> {
-//                results = entities.filter { it.date == timeLine } as ArrayList<Order>
-//                complete(results)
-//            }
-            else -> {
-            }
+            TimeUnit.day -> filterByDay(entities, timeLine, complete)
+            TimeUnit.weak -> filterByWeak(entities, timeLine, complete)
+            TimeUnit.month -> filterByMonth(entities, timeLine, complete)
+//            TimeUnit.year -> filterByYear(entities, timeLine, complete)
+            else -> failure()
         }
     }
+
+    private fun filterByDay(entities: ArrayList<Order>,
+                            timeLine: Date,
+                            complete: (ArrayList<Order>) -> Unit) {
+        val results = entities.filter { timeLine.compareDate(it.date) } as ArrayList<Order>
+        complete(results)
+    }
+
+    private fun filterByWeak(entities: ArrayList<Order>,
+                             timeLine: Date,
+                             complete: (ArrayList<Order>) -> Unit) {
+        // filter by weak: Mon - Sun
+        // get list order in a day -> totalPrice all order in day
+        val results: ArrayList<Order> = ArrayList()
+        val daysOfWeek = timeLine.getDaysOfWeek()
+        daysOfWeek.forEach {
+                val carts = getCartInDay(entities, it.getDayOfDate().toInt())
+                results.add(Order("", "", carts, it))
+        }
+
+        complete(results)
+    }
+
+    private fun filterByMonth(entities: ArrayList<Order>,
+                              timeLine: Date,
+                              complete: (ArrayList<Order>) -> Unit) {
+        // filter by month: 1 - 31
+        // get list order in a day -> totalPrice all order in day
+
+        val entitiesInMonth = entities.filter { it.date.compareMonth(timeLine) && it.date.compareYear(timeLine) } as ArrayList<Order>
+        val results: ArrayList<Order> = ArrayList()
+
+        (1..31).forEach { day ->
+            val carts = getCartInDay(entitiesInMonth, day)
+
+            val calendar = Calendar.getInstance()
+            calendar.set(
+                timeLine.getYearOfDate().toInt(),
+                timeLine.getMonthOfDate().toInt() - 1,
+                day
+            )
+            results.add(Order("", "", carts, calendar.time))
+        }
+
+        complete(results)
+    }
+
+    private fun getCartInDay(entitiesInMonth: ArrayList<Order>, day: Int): ArrayList<Cart> {
+        val entitiesInDay = entitiesInMonth.filter { it.date.getDayOfDate().toInt() == day }
+        val carts = ArrayList<Cart>()
+        entitiesInDay.forEach {
+            carts.addAll(it.carts)
+        }
+        return carts
+    }
+
+    // TODO: I'll update get year after
+//    private fun filterByYear(entities: ArrayList<Order>,
+//                             timeLine: Date,
+//                             complete: (ArrayList<Order>) -> Unit) {
+//        // filter all year:
+//        // get array(Order) in a year from database
+//        // get total price in a year
+//
+//        val entitiesInYear = entities.filter { it.date.compareYear(timeLine) } as ArrayList<Order>
+//
+//        val results: ArrayList<Order> = ArrayList()
+//
+//        (1..12).forEach { month ->
+//            val entitiesInMonth = entitiesInYear.filter { it.date.getMonthOfDate() == month.toString() }
+//            val carts = ArrayList<Cart>()
+//            entitiesInMonth.forEach {
+//                carts.addAll(it.carts)
+//            }
+//
+//            val calendar = Calendar.getInstance()
+//            calendar.set(
+//                timeLine.getYearOfDate().toInt(),
+//                month-1,
+//                timeLine.getDayOfDate().toInt() - 1
+//            )
+//            results.add(Order("", "", carts, calendar.time))
+//        }
+//
+//        complete(results)
+//    }
 }
--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/statistics/presentations/StatisticsFragment.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/statistics/presentations/StatisticsFragment.kt
@@ -6,23 +6,22 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.ArrayAdapter
-import android.widget.ImageButton
-import android.widget.ProgressBar
-import android.widget.Spinner
-import android.widget.TextView
+import android.widget.*
+import android.widget.AdapterView.OnItemSelectedListener
 import androidx.fragment.app.Fragment
 import com.github.mikephil.charting.charts.BarChart
+import com.github.mikephil.charting.components.XAxis
 import com.github.mikephil.charting.data.BarData
 import com.github.mikephil.charting.data.BarDataSet
 import com.github.mikephil.charting.data.BarEntry
+import com.github.mikephil.charting.formatter.IndexAxisValueFormatter
 import com.tavanhieu.quanlytaphoa.R
 import com.tavanhieu.quanlytaphoa.activities.statistics.domain.enum.TimeUnit
 import com.tavanhieu.quanlytaphoa.activities.statistics.domain.infra.StatisticUseCaseImpl
 import com.tavanhieu.quanlytaphoa.activities.statistics.domain.use_cases.StatisticUseCase
+import com.tavanhieu.quanlytaphoa.commons.*
 import com.tavanhieu.quanlytaphoa.commons.base.BaseActivity
 import com.tavanhieu.quanlytaphoa.commons.base.showAlertDialog
-import com.tavanhieu.quanlytaphoa.commons.formatDate
 import com.tavanhieu.quanlytaphoa.commons.models.Order
 import java.util.Calendar
 
@@ -35,8 +34,7 @@ class StatisticsFragment(val context: BaseActivity): Fragment() {
 
     private val timeLineDate: Calendar by lazy { Calendar.getInstance() }
     private val statisticUseCase: StatisticUseCase by lazy { StatisticUseCaseImpl() }
-    private var barEntriesList: ArrayList<BarEntry> = ArrayList()
-    private val timeUnits = listOf("Ngày", "Tuần", "Tháng", "Năm")
+    private val timeUnits = listOf("Ngày", "Tuần", "Tháng")
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -70,32 +68,48 @@ class StatisticsFragment(val context: BaseActivity): Fragment() {
 
     private fun loadDataForStatistic() {
         progressBar.visibility = View.VISIBLE
-        statisticUseCase.filterOrderListBy(timeLineDate.time, TimeUnit.day, {
-            handleFilterOrderListSuccess(it)
+        val timeUnit: TimeUnit = when (timeUnitSpinner.selectedItemId) {
+            0L -> TimeUnit.day
+            1L -> TimeUnit.weak
+            2L -> TimeUnit.month
+//            3L -> TimeUnit.year
+            else -> TimeUnit.day
+        }
+
+        statisticUseCase.filterOrderListBy(timeLineDate.time, timeUnit, {
+            handleFilterOrderListSuccess(it, timeUnit)
         }, {
             handleFilterOrderListFailure()
         })
     }
 
-    private fun handleFilterOrderListSuccess(orders: ArrayList<Order>) {
-        progressBar.visibility = View.GONE
-//        barEntriesList = orders.flatMap { order -> BarEntry(order.totalPrice(), "1") }
+    private fun handleFilterOrderListSuccess(orders: ArrayList<Order>, timeUnit: TimeUnit) {
+        context.showToast(timeUnit.toString())
+        val barEntriesList: ArrayList<BarEntry> = ArrayList()
+        val labels: ArrayList<String> = ArrayList()
+        var i = 0f
         orders.forEach {
-            barEntriesList.add(BarEntry(it.totalPrice(), it.id.toFloat()))
+            barEntriesList.add(BarEntry(i++, it.totalPrice()))
+            when (timeUnit) {
+                TimeUnit.day -> labels.add(it.date.formatTime())
+                TimeUnit.weak -> labels.add(it.date.getNameOfDayInWeek())
+                TimeUnit.month -> labels.add(it.date.getNameOfDayAndMonth())
+                TimeUnit.year -> labels.add(it.date.getNameOfMonth())
+                else -> labels.add(it.date.formatTime())
+            }
         }
-        val barDataSet = BarDataSet(barEntriesList, "Bar Chart Data")
+
+        progressBar.visibility = View.GONE
+        val barDataSet = BarDataSet(barEntriesList, context.getResourceText(R.string.revenueStatistics))
         val barData = BarData(barDataSet)
         barChart.data = barData
+        barChart.xAxis.valueFormatter = IndexAxisValueFormatter(labels)
+        barChart.xAxis.setCenterAxisLabels(true)
+        barChart.xAxis.enableGridDashedLine(0f, 5f, 0f)
+        barChart.description.isEnabled = false
+        barChart.animateY(1500)
+        barChart.setFitBars(true)
     }
-
-//    // on below line we are setting colors for our bar chart text
-//    barDataSet.valueTextColor = Color.BLACK
-//    // on below line we are setting color for our bar data set
-//    barDataSet.setColor(resources.getColor(R.color.purple_200))
-//    // on below line we are setting text size
-//    barDataSet.valueTextSize = 16f
-//    // on below line we are enabling description as false
-//    barChart.description.isEnabled = false
 
     private fun handleFilterOrderListFailure() {
         progressBar.visibility = View.GONE
@@ -110,13 +124,17 @@ class StatisticsFragment(val context: BaseActivity): Fragment() {
 
     private fun handleClickOnView() {
         calendarImageButton.setOnClickListener { openDatePicker()  }
+        timeUnitSpinner.onItemSelectedListener = object: OnItemSelectedListener {
+            override fun onItemSelected(p0: AdapterView<*>?, p1: View?, p2: Int, p3: Long) {
+                loadDataForStatistic()
+            }
+            override fun onNothingSelected(p0: AdapterView<*>?) {}
+        }
     }
 
     @SuppressLint("SimpleDateFormat")
     private fun openDatePicker() {
-        DatePickerDialog(
-            requireContext(),
-            { _, i, i2, i3 ->
+        DatePickerDialog(requireContext(), { _, i, i2, i3 ->
                 timeLineDate.set(Calendar.YEAR, i)
                 timeLineDate.set(Calendar.MONTH, i2)
                 timeLineDate.set(Calendar.DAY_OF_MONTH, i3)
