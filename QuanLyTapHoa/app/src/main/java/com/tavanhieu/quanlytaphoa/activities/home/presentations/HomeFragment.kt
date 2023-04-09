4670cdf3aa81e13047f015c34a44d35d3fac25c6
hi-uta27 <tavanhieun@gmail.com>
2023-04-09 21:20:47 +0700

    open search acitivity

--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/home/presentations/HomeFragment.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/home/presentations/HomeFragment.kt
@@ -1,18 +1,22 @@
 package com.tavanhieu.quanlytaphoa.activities.home.presentations
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.tavanhieu.quanlytaphoa.R
 import com.tavanhieu.quanlytaphoa.activities.home.adapters.FunctionItemAdapter
 import com.tavanhieu.quanlytaphoa.activities.home.models.FunctionItem
+import com.tavanhieu.quanlytaphoa.activities.search.presentations.SearchActivity
 
 class HomeFragment: Fragment() {
     private lateinit var nameEmployeeTextView: TextView
+    private lateinit var searchImageView: ImageView
     private lateinit var recyclerViewFunction: RecyclerView
     private lateinit var recyclerViewProduct: RecyclerView
 
@@ -26,21 +30,29 @@ class HomeFragment: Fragment() {
     ): View {
         val view = LayoutInflater.from(requireContext()).inflate(R.layout.fragment_home, container, false)
         mappingViewId(view)
+        handleClickOnView()
         displayFunctionRecyclerView()
 
         return view
-    }
-
-    private fun displayFunctionRecyclerView() {
-        getArr()
-        adapter.setData(arr)
-        recyclerViewFunction.adapter = adapter
     }
 
     private fun mappingViewId(view: View) {
         nameEmployeeTextView = view.findViewById(R.id.nameEmployeeTextView)
         recyclerViewFunction = view.findViewById(R.id.recycleViewFunction)
         recyclerViewProduct = view.findViewById(R.id.recycleViewProduct)
+        searchImageView = view.findViewById(R.id.searchImageView)
+    }
+
+    private fun handleClickOnView() {
+        searchImageView.setOnClickListener {
+            requireActivity().startActivity(Intent(requireActivity(), SearchActivity::class.java))
+        }
+    }
+
+    private fun displayFunctionRecyclerView() {
+        getArr()
+        adapter.setData(arr)
+        recyclerViewFunction.adapter = adapter
     }
 
     private fun getArr() {
