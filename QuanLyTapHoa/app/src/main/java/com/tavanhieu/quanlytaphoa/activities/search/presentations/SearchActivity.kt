1544c1762568ba10d0321ff307abb5d398aa007b
hi-uta27 <tavanhieun@gmail.com>
2023-04-09 21:08:01 +0700

    apply search use case to search product

--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/search/presentations/SearchActivity.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/search/presentations/SearchActivity.kt
@@ -1,16 +1,28 @@
 package com.tavanhieu.quanlytaphoa.activities.search.presentations
 
+import android.view.View
 import android.widget.ImageView
+import android.widget.ProgressBar
 import android.widget.SearchView
+import android.widget.SearchView.OnQueryTextListener
 import androidx.recyclerview.widget.RecyclerView
 import com.tavanhieu.quanlytaphoa.R
+import com.tavanhieu.quanlytaphoa.activities.depot.adapter.DepotAdapter
+import com.tavanhieu.quanlytaphoa.activities.search.domain.infra.SearchUseCaseImpl
+import com.tavanhieu.quanlytaphoa.activities.search.domain.use_cases.SearchUseCase
 import com.tavanhieu.quanlytaphoa.commons.base.BaseActivity
+import com.tavanhieu.quanlytaphoa.commons.base.showAlertDialog
+import com.tavanhieu.quanlytaphoa.commons.models.Product
 
 class SearchActivity : BaseActivity() {
     private lateinit var imageBack: ImageView
     private lateinit var scanBarCodeImageView: ImageView
     private lateinit var searchView: SearchView
     private lateinit var recyclerView: RecyclerView
+    private lateinit var progressBar: ProgressBar
+
+    private val searchUseCase: SearchUseCase by lazy { SearchUseCaseImpl() }
+    private val productAdapter: DepotAdapter by lazy { DepotAdapter() }
 
     override fun setContentView() {
         setContentView(R.layout.activity_search)
@@ -19,11 +31,51 @@ class SearchActivity : BaseActivity() {
     override fun mappingViewId() {
         imageBack = findViewById(R.id.imageBack)
         searchView = findViewById(R.id.searchView)
+        progressBar = findViewById(R.id.progressBar)
         recyclerView = findViewById(R.id.recyclerView)
         scanBarCodeImageView = findViewById(R.id.scanBarCodeImageView)
     }
 
     override fun configLayout() {
-        //
+        imageBack.setOnClickListener { finish() }
+        searchView.setOnQueryTextListener(object: OnQueryTextListener {
+            override fun onQueryTextSubmit(p0: String?): Boolean {
+                return true
+            }
+
+            override fun onQueryTextChange(p0: String?): Boolean {
+                if (p0 != null) {
+                    searchWithName(p0)
+                }
+                return true
+            }
+        })
+
+    }
+
+    private fun searchWithName(name: String) {
+        progressBar.visibility = View.VISIBLE
+        searchUseCase.searchProductByName(name, {
+            handleSearchWithNameSuccess(it)
+        }, {
+            handleSearchFailure()
+        })
+    }
+
+    private fun handleSearchWithNameSuccess(products: ArrayList<Product>) {
+        progressBar.visibility = View.GONE
+        productAdapter.setData(this, products)
+        recyclerView.adapter = productAdapter
+    }
+
+    private fun handleSearchFailure() {
+        progressBar.visibility = View.GONE
+        showAlertDialog(
+            getResourceText(R.string.error),
+            getResourceText(R.string.searchFailure),
+            getResourceText(R.string.tryAgain)
+        ) {
+            searchWithName(searchView.query.toString())
+        }
     }
 }
