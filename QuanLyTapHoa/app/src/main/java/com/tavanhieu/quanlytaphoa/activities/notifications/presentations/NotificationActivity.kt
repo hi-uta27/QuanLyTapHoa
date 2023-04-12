731c7b6a41c6cc67b71cfb7f5397bf6ccec66f12
hi-uta27 <tavanhieun@gmail.com>
2023-04-12 21:41:03 +0700

    update display notification

--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/notifications/presentations/NotificationActivity.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/notifications/presentations/NotificationActivity.kt
@@ -1,13 +1,20 @@
 package com.tavanhieu.quanlytaphoa.activities.notifications.presentations
 
 import android.annotation.SuppressLint
+import android.app.Notification
+import android.app.NotificationChannel
+import android.app.NotificationManager
 import android.app.PendingIntent
+import android.content.Context
 import android.content.Intent
 import android.graphics.BitmapFactory
+import android.graphics.Color
+import android.os.Build
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import com.tavanhieu.quanlytaphoa.R
 import com.tavanhieu.quanlytaphoa.activities.depot.presentations.DepotActivity
+import com.tavanhieu.quanlytaphoa.activities.detail_product.presentations.DetailProductActivity
 import com.tavanhieu.quanlytaphoa.activities.notifications.domain.use_cases.NotificationsUseCase
 import com.tavanhieu.quanlytaphoa.commons.base.BaseActivity
 import com.tavanhieu.quanlytaphoa.commons.models.Product
@@ -15,33 +22,35 @@ import com.tavanhieu.quanlytaphoa.commons.models.Product
 interface NotificationActivity {
     val notificationsUseCase: NotificationsUseCase
     val context: BaseActivity
-//    private lateinit var notificationManager: NotificationManager
-//    private lateinit var notificationChannel: NotificationChannel
-//    private lateinit var builder: NotificationCompat.Builder
 
     // ---------------------------------------------------------------------
 
     @SuppressLint("UnspecifiedImmutableFlag")
-    private fun displayNotification(message: String) {
-        val intent = Intent(context, DepotActivity::class.java)
+    private fun displayNotification(message: String, product: Product) {
+        val notificationChannel: NotificationChannel
+        val builder: NotificationCompat.Builder
+        val notificationManager: NotificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+        val channelId = "NOTIFICATIONS_QUAN_LY_BAN_HANG"
+
+        val intent = Intent(context, DetailProductActivity::class.java)
+        intent.putExtra("IdProduct", product.id)
         val pendingIntent = PendingIntent.getActivity(context, 0, intent, PendingIntent.FLAG_UPDATE_CURRENT)
-
         // checking if android version is greater than oreo(API 26) or not
-//        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-//            notificationChannel = NotificationChannel(chanelId, chanelId, NotificationManager.IMPORTANCE_HIGH)
-//            notificationChannel.enableLights(true)
-//            notificationChannel.enableVibration(false)
-//            notificationManager.createNotificationChannel(notificationChannel)
-//        }
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            notificationChannel = NotificationChannel(channelId, channelId, NotificationManager.IMPORTANCE_HIGH)
+            notificationChannel.enableLights(true)
+            notificationChannel.lightColor = Color.GREEN
+            notificationChannel.enableVibration(false)
+            notificationManager.createNotificationChannel(notificationChannel)
+        }
 
-        val chanelId = "NOTIFICATIONS_QUAN_LY_BAN_HANG"
-        val builder = NotificationCompat.Builder(context, chanelId)
+        builder = NotificationCompat.Builder(context, channelId)
             .setSmallIcon(R.drawable.ic_logo)
-            .setContentTitle(context.getResourceText(R.string.title_app))
+            .setContentTitle(product.name)
             .setContentText(message)
             .setLargeIcon(BitmapFactory.decodeResource(context.resources, R.drawable.ic_logo))
             .setContentIntent(pendingIntent)
-
+            .setAutoCancel(true)
         NotificationManagerCompat.from(context).notify(1234, builder.build())
     }
 
@@ -54,8 +63,10 @@ interface NotificationActivity {
     }
 
     private fun checkExpiredDateOfProductSuccess(products: ArrayList<Product>) {
-        if (products.size != 0) {
-            displayNotification(context.getResourceText(R.string.productExpiredDate))
+        products.forEach {
+            displayNotification(context.getResourceText(R.string.productExpiredDate), it)
         }
     }
+
+    // ---------------------------------------------------------------------
 }
