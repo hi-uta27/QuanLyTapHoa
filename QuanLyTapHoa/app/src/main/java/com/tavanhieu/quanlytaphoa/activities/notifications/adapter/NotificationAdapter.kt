ddc09d9123f19e0878192959875066d346231516
hi-uta27 <tavanhieun@gmail.com>
2023-04-18 22:13:42 +0700

    update notification adapter

--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/notifications/adapter/NotificationAdapter.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/notifications/adapter/NotificationAdapter.kt
@@ -1,7 +1,6 @@
 package com.tavanhieu.quanlytaphoa.activities.notifications.adapter
 
 import android.annotation.SuppressLint
-import android.content.Intent
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,22 +8,16 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.tavanhieu.quanlytaphoa.R
-import com.tavanhieu.quanlytaphoa.activities.detail_order.presentations.DetailOrderActivity
-import com.tavanhieu.quanlytaphoa.activities.detail_product.presentations.DetailProductActivity
 import com.tavanhieu.quanlytaphoa.commons.base.BaseActivity
-import com.tavanhieu.quanlytaphoa.commons.formatCurrency
-import com.tavanhieu.quanlytaphoa.commons.formatDate
 import com.tavanhieu.quanlytaphoa.commons.formatDateAndTime
-import com.tavanhieu.quanlytaphoa.commons.formatTime
-import com.tavanhieu.quanlytaphoa.commons.models.Employee
 import com.tavanhieu.quanlytaphoa.commons.models.Notification
-import com.tavanhieu.quanlytaphoa.commons.models.Order
-import com.tavanhieu.quanlytaphoa.data_network_layer.FirebaseNetworkLayer
-import java.text.SimpleDateFormat
 
 class NotificationAdapter : RecyclerView.Adapter<NotificationAdapter.NotificationAdapter>(){
     private lateinit var arr: ArrayList<Notification>
     private lateinit var context: BaseActivity
+
+    var touchUpInsideItemView: ((Notification) -> Unit)? = null
+    var touchUpInsideDeleteImageView: ((Notification) -> Unit)? = null
 
     @SuppressLint("NotifyDataSetChanged")
     fun setData(context: BaseActivity, arr: ArrayList<Notification>) {
@@ -37,12 +30,23 @@ class NotificationAdapter : RecyclerView.Adapter<NotificationAdapter.Notificatio
         private var titleTextView: TextView = itemView.findViewById(R.id.titleTextView)
         private var messageTextView: TextView = itemView.findViewById(R.id.messageTextView)
         private var dateTextView: TextView = itemView.findViewById(R.id.dateTextView)
+        private var deleteImageView: ImageView = itemView.findViewById(R.id.deleteImageView)
 
         @SuppressLint("SetTextI18n", "SimpleDateFormat")
         fun binding(notification: Notification) {
             titleTextView.text = notification.title
             messageTextView.text = notification.message
             dateTextView.text = notification.date.formatDateAndTime()
+        }
+
+        fun handleClickOnView(notification: Notification) {
+            itemView.setOnClickListener {
+                touchUpInsideItemView?.invoke(notification)
+            }
+
+            deleteImageView.setOnClickListener {
+                touchUpInsideDeleteImageView?.invoke(notification)
+            }
         }
     }
 
@@ -54,11 +58,7 @@ class NotificationAdapter : RecyclerView.Adapter<NotificationAdapter.Notificatio
     override fun onBindViewHolder(holder: NotificationAdapter, position: Int) {
         val notification = arr[position]
         holder.binding(notification)
-        holder.itemView.setOnClickListener {
-            val intent = Intent(context, DetailProductActivity::class.java)
-            intent.putExtra("IdProduct", notification.idProduct)
-            context.startActivity(intent)
-        }
+        holder.handleClickOnView(notification)
     }
 
     override fun getItemCount(): Int {
