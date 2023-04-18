1f73967d09abc5f1c2e1247cee47eda3fbf60366
hi-uta27 <tavanhieun@gmail.com>
2023-04-18 22:14:19 +0700

    apply delete notification

--- QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/notifications/presentations/NotificationFragment.kt
+++ QuanLyTapHoa/app/src/main/java/com/tavanhieu/quanlytaphoa/activities/notifications/presentations/NotificationFragment.kt
@@ -1,5 +1,6 @@
 package com.tavanhieu.quanlytaphoa.activities.notifications.presentations
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
@@ -9,6 +10,7 @@ import android.widget.TextView
 import androidx.fragment.app.Fragment
 import androidx.recyclerview.widget.RecyclerView
 import com.tavanhieu.quanlytaphoa.R
+import com.tavanhieu.quanlytaphoa.activities.detail_product.presentations.DetailProductActivity
 import com.tavanhieu.quanlytaphoa.activities.notifications.adapter.NotificationAdapter
 import com.tavanhieu.quanlytaphoa.activities.notifications.domain.infra.NotificationUseCaseImpl
 import com.tavanhieu.quanlytaphoa.activities.notifications.domain.use_cases.NotificationsUseCase
@@ -32,6 +34,7 @@ class NotificationFragment(val context: BaseActivity) : Fragment() {
         val view = LayoutInflater.from(context).inflate(R.layout.fragment_notifications, container, false)
         mappingViewId(view)
         readNotification()
+        handleClickOnView()
 
         return view
     }
@@ -40,6 +43,43 @@ class NotificationFragment(val context: BaseActivity) : Fragment() {
         recycleView = view.findViewById(R.id.recycleView)
         progressBar = view.findViewById(R.id.progressBar)
         emptyTextView = view.findViewById(R.id.emptyTextView)
+    }
+
+    private fun handleClickOnView() {
+        adapter.touchUpInsideItemView = { notification ->
+            val intent = Intent(context, DetailProductActivity::class.java)
+            intent.putExtra("IdProduct", notification.idProduct)
+            context.startActivity(intent)
+        }
+
+        adapter.touchUpInsideDeleteImageView = { notification ->
+            deleteNotification(notification)
+        }
+    }
+
+    private fun deleteNotification(notification: Notification) {
+        context.showAlertDialog(
+            context.getResourceText(R.string.notification),
+            context.getResourceText(R.string.deleteNotification),
+            context.getResourceText(R.string.confirm)
+        ) {
+            progressBar.visibility = View.VISIBLE
+            notificationsUseCase.deleteNotification(notification, {
+                deleteNotificationSuccess()
+            }, {
+                deleteNotificationFailure()
+            })
+        }
+    }
+
+    private fun deleteNotificationSuccess() {
+        progressBar.visibility = View.GONE
+        context.showToast(context.getResourceText(R.string.deleteNotificationSuccess))
+    }
+
+    private fun deleteNotificationFailure() {
+        progressBar.visibility = View.GONE
+        context.showAlertDialog(context.getResourceText(R.string.error), context.getResourceText(R.string.deleteNotificationFailure)) {}
     }
 
     private fun readNotification() {
@@ -65,7 +105,7 @@ class NotificationFragment(val context: BaseActivity) : Fragment() {
     private fun readNotificationFailure() {
         progressBar.visibility = View.GONE
         context.showAlertDialog(context.getResourceText(R.string.error),
-            context.getResourceText(R.string.readDepotFailure),
+            context.getResourceText(R.string.readNotificationFailure),
             context.getResourceText(R.string.tryAgain)
         ) {
             readNotification()
